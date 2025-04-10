3d01ba56e0208a69b03a2f84820b6cdf3269bb15
bum0w0 <kgy5988@naver.com>
2025-04-11 00:26:03 +0900

    Entity 클래스 개발 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,18 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable // 내장 타입 (다른 엔티티에 내장되어 사용됨)
 @Getter
+// JPA 스펙에 따라 엔티티나 임베디드 타입에는 기본 생성자가 반드시 있어야 하며, 해당 생성자는 public 또는 protected로 선언해야 함
+// 일반적으로는 protected로 설정하여 외부에서의 무분별한 객체 생성을 방지하는 것이 좋다. (필요한 경우에는 public 생성자를 사용해야 한다는 인지 또한 가능하게 함)
+// JPA에서 이러한 제약을 두는 이유는, 내부적으로 객체를 생성할 때 리플렉션(reflection)과 같은 기술을 사용하기 때문
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Address {
 
     private String city;
---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    // 다대다 관계를 풀기 위해 중간 테이블이 필요
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
+    // JPA에서 자기 자신을 참조하는 양방향 연관관계 (자기 자신과 부모-자식 관계를 맺는 구조)
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+}
