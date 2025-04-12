1f4c941e5ebef8a7eaf917d175dc3b7bbaba5518
bum0w0 <kgy5988@naver.com>
2025-04-12 21:07:11 +0900

    회원 기능 테스트

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +11,8 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor
+@Builder
 public class Member {
 
     @Id @GeneratedValue
@@ -22,5 +26,10 @@ public class Member {
 
     @OneToMany(mappedBy = "member") // mappedBy로 연관관계의 주인이 아님을 명시, 실제 외래 키(FK)는 반대쪽(Order.member)에서 관리된다
     private List<Order> orders = new ArrayList<>();
+
+    // @Builder는 내부적으로 전체 필드를 받는 생성자를 만들어 쓰기 때문에, JPA가 사용하는 기본 생성자도 필요
+    protected Member() {
+
+    }
 
 }
