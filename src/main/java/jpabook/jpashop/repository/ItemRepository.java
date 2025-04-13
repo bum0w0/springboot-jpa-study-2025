86234956ae8af0c5a25c1ee9cb700fb8f19f8285
bum0w0 <kgy5988@naver.com>
2025-04-14 00:01:47 +0900

    상품 레포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item); // id == null → 새 엔티티이므로 DB에 insert 필요 → persist
+        } else {
+            em.merge(item); // 	id != null → 이미 저장된 엔티티일 가능성 → 상태를 갱신해야 하므로 merge
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
