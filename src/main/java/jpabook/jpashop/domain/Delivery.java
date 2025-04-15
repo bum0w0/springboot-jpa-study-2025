7dcdc8ad5cb2e4dcf4940993240ff57e4aecf673
bum0w0 <kgy5988@naver.com>
2025-04-16 02:06:46 +0900

    주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@Builder
 public class Delivery {
 
     @Id @GeneratedValue
@@ -22,5 +24,9 @@ public class Delivery {
     // 기본값인 ORDINAL을 사용하면 enum 순서 변경이나 값 추가 시 기존 데이터 매핑이 틀어질 수 있으므로, 안정성을 위해 STRING을 사용하는 것이 좋다.
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status; // 배송상태 [READY, COMP]
+
+    protected Delivery() {
+
+    }
 
 }
