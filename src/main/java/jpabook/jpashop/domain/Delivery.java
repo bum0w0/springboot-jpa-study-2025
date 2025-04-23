cde279575411e78586665d563ed0232ecbb87bbd
bum0w0 <kgy5988@naver.com>
2025-04-23 19:07:40 +0900

    조회용 샘플 데이터 입력

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,14 +1,12 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Delivery {
 
@@ -26,9 +24,5 @@ public class Delivery {
     // 기본값인 ORDINAL을 사용하면 enum 순서 변경이나 값 추가 시 기존 데이터 매핑이 틀어질 수 있으므로, 안정성을 위해 STRING을 사용하는 것이 좋다.
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status; // 배송상태 [READY, COMP]
-
-    protected Delivery() {
-
-    }
 
 }
