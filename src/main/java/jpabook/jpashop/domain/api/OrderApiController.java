a7898bfe19ae91c2b3ac9fbc04625b2b7496186a
bum0w0 <kgy5988@naver.com>
2025-04-28 21:05:29 +0900

    엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/domain/api/OrderApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 이 컨트롤러는 컬렉션(xToMany) 관계에서 조회 성능 최적화를 어떻게 해야 하는지를 다룬다.
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            // LAZY 강제 초기화
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+
+        return all;
+    }
+
+}
