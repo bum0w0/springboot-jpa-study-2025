59c3b01e273140406c3e7eb8290c09b7a4d7f0e9
bum0w0 <kgy5988@naver.com>
2025-05-07 01:17:11 +0900

    QueryDSL 소개
    QueryDSL 기반 로직으로 변경

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -61,7 +61,9 @@ public class OrderService {
 
     // 주문 검색
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAllByString(orderSearch);
+        return orderRepository.findAll(orderSearch);
     }
+
+
 
 }
